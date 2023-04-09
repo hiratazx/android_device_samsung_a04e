5fb2efae8890c78c820818675e23e663658577b2
Hirozuto <madeayurenapramudya@gmail.com>
2023-04-09 05:38:09 +0000

    a04e: Import releasetools from https://gitlab.com/ItzKaguya/android_device_samsung_a10s/-/tree/unstable/lineage-17.1/releasetools
    Signed-off-by: Raka Prasetya Pratama <madeayurenapramudya@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,5 +101,8 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
+
 # Inherit the proprietary files
 include vendor/samsung/a04e/BoardConfigVendor.mk
